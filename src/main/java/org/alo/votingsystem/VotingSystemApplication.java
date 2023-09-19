422a1263c8a21c002e2abf63803beec925828e1b
Gruzin <grzegorz.jagielski.2019@zsa.pwr.edu.pl>
2023-09-19 03:41:04 +0200

    Fixing project metadata

--- src/main/java/org/alo/votingsystem/VotingSystemApplication.java
+++ src/main/java/org/alo/votingsystem/VotingSystemApplication.java
@@ -6,6 +6,9 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class VotingSystemApplication {
 
+	// TODO: Content-Type: application/x-www-form-urlencoded doesn't work in whole application
+	// Additionally on POST /login:
+	// Resolved [org.springframework.web.HttpMediaTypeNotSupportedException: Content-Type 'application/x-www-form-urlencoded;charset=UTF-8' is not supported]
 	public static void main(String[] args) {
 		SpringApplication.run(VotingSystemApplication.class, args);
 	}
